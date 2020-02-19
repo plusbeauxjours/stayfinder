8a4de9b3ffd2020a1b0530757e83a05ee682b149
plusbeauxjours <plusbeauxjours@gmail.com>
2020-02-19 11:48:55 +0900

    seed_users

---
+++ core/management/commands/seed_facilities.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from rooms.models import Facility
+
+
+class Command(BaseCommand):
+
+    help = "This command creates many facilities"
+
+    # def Fdd_argfacilitiesmnets(self, parser):
+    #     parser.add_argument(
+    #         "--number", help="How many facilities do you want to create")
+    #     )
+
+    def handle(self, *args, **options):
+        facilities = [
+            "Private entrance",
+            "Paid parking on premises",
+            "Paid parking off premises",
+            "Elevator",
+            "Parking",
+            "Gym",
+        ]
+
+        for f in facilities:
+            Facility.objects.create(name=f)
+        self.stdout.write(
+            self.style.SUCCESS(f"{len(facilities)} Facilitiessers created")
+        )
