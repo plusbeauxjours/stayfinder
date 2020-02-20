deac2f2b8474f7fa2dfc357c10daee8ceb33c5a7
plusbeauxjours <plusbeauxjours@gmail.com>
2020-02-20 19:44:22 +0900

    roomDetail FBV

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -22,9 +22,6 @@ class Command(BaseCommand):
         seeder = Seed.seeder()
         all_users = user_models.User.objects.all()
         all_room_types = room_models.RoomType.objects.all()
-        all_amenities = room_models.Amenity.objects.all()
-        all_facilities = room_models.Facility.objects.all()
-        all_rules = room_models.HouseRule.objects.all()
         seeder.add_entity(
             room_models.Room,
             number,
@@ -41,6 +38,9 @@ class Command(BaseCommand):
         )
         created_photos = seeder.execute()
         created_clean = flatten(list(created_photos.values()))
+        all_amenities = room_models.Amenity.objects.all()
+        all_facilities = room_models.Facility.objects.all()
+        all_rules = room_models.HouseRule.objects.all()
         for pk in created_clean:
             room = room_models.Room.objects.get(pk=pk)
             for i in range(3, random.randint(10, 17)):
@@ -49,16 +49,16 @@ class Command(BaseCommand):
                     room=room,
                     file=f"room_photos/{random.randint(1,17)}.webp",
                 )
-        for a in all_amenities:
-            magic_number = random.randint(0, 15)
-            if magic_number % 2 == 0:
-                room.amenities.add(a)
-        for f in all_facilities:
-            magic_number = random.randint(0, 15)
-            if magic_number % 2 == 0:
-                room.facilities.add(f)
-        for r in all_rules:
-            magic_number = random.randint(0, 15)
-            if magic_number % 2 == 0:
-                room.rules.add(r)
-        self.stdout.write(self.style.SUCCESS(f"{number} {NAME} created"))
+            for a in all_amenities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.amenities.add(a)
+            for f in all_facilities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.facilities.add(f)
+            for r in all_rules:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.house_rules.add(r)
+        self.stdout.write(self.style.SUCCESS(f"{number} {NAME} created!"))
