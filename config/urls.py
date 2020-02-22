b2ed337f1f4c3e27a2ced936136e06cb1de7d722
plusbeauxjours <plusbeauxjours@gmail.com>
2020-02-22 10:07:28 +0900

    CSRF

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
+    path("users/", include("users.urls", namespace="usrs")),
     path("rooms/", include("rooms.urls", namespace="rooms")),
     path("admin/", admin.site.urls),
 ]
