4439cb0780ae09bf1b6541b35f990904c42929aa
plusbeauxjours <plusbeauxjours@gmail.com>
2020-03-01 17:21:48 +0900

    roomCalendar

--- rooms/models.py
+++ rooms/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
+from cal import Calendar
 
 
 class AbstractItem(core_models.TimeStampedModel):
@@ -136,3 +137,7 @@ class Room(core_models.TimeStampedModel):
         photos = self.photos.all()[1:5]
         return photos
 
+    def get_calendars(self):
+        this_month = Calendar(2019, 11)
+        next_month = Calendar(2019, 12)
+        return [this_month, next_month]
