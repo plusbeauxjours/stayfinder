d85579e3738d8d7a0c9f38c3738749b89faf1311
plusbeauxjours <plusbeauxjours@gmail.com>
2020-03-08 15:15:47 +0900

    again

--- config/wsgi.py
+++ config/wsgi.py
@@ -13,6 +13,8 @@ framework.
 import os
 import sys
 
+import dotenv
+
 from django.core.wsgi import get_wsgi_application
 
 
@@ -23,6 +25,7 @@ app_path = os.path.abspath(
 )
 sys.path.append(os.path.join(app_path, "airbnp"))
 
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
