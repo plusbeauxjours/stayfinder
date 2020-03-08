aef3ebbeefc7154889494f6d8ae03be2b9193b02
plusbeauxjours <plusbeauxjours@gmail.com>
2020-03-08 22:38:14 +0900

    createSuperUser

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET", "NTfF6fEHnYx^P6@HJx@K6M")
+SECRET_KEY = os.environ.get("DJANGO_SECRET", "NTfF6fDHnYx^P6@HJx@K6M")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG"))
