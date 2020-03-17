538aa2faf1ec2f49dc7cafc875bc5c354bdf3f96
plusbeauxjours <plusbeauxjours@gmail.com>
2020-03-17 18:52:25 +0900

    remove facebookLogin

--- users/views.py
+++ users/views.py
@@ -206,78 +206,78 @@ def kakao_callback(request):
         return redirect(reverse("users:login"))
 
 
-def facebook_login(request):
-    client_id = os.environ.get("FACEBOOK_ID")
-    redirect_uri = "http://airbnp.eba-8dydnzzj.ap-northeast-2.elasticbeanstalk.com/users/login/facebook/callback"
-    response_type = "code"
-    return redirect(
-        f"https://www.facebook.com/v6.0/dialog/oauth?client_id={client_id}&redirect_uri={redirect_uri}&response_type={response_type}",
-    )
+# def facebook_login(request):
+#     client_id = os.environ.get("FACEBOOK_ID")
+#     redirect_uri = "http://airbnp.eba-8dydnzzj.ap-northeast-2.elasticbeanstalk.com/users/login/facebook/callback"
+#     response_type = "code"
+#     return redirect(
+#         f"https://www.facebook.com/v6.0/dialog/oauth?client_id={client_id}&redirect_uri={redirect_uri}&response_type={response_type}",
+#     )
 
 
-class FacebookException(Exception):
-    pass
+# class FacebookException(Exception):
+#     pass
 
 
-def facebook_callback(request):
-    try:
-        client_id = os.environ.get("FACEBOOK_ID")
-        client_secret = os.environ.get("FACEBOOK_SECRET")
-        redirect_uri = "http://airbnp.eba-8dydnzzj.ap-northeast-2.elasticbeanstalk.com/users/login/facebook/callback"
-        code = request.GET.get("code", None)
-        if code is not None:
-            token_request = requests.post(
-                f"https://graph.facebook.com/v6.0/oauth/access_token?client_id={client_id}&redirect_uri={redirect_uri}&client_secret={client_secret}&code={code}",
-                headers={"Accept": "application/json"},
-            )
-            token_json = token_request.json()
-            error = token_json.get("error", None)
-            if error is not None:
-                raise FacebookException("Can't get access token")
-            else:
-                url_user_info = 'https://graph.facebook.com/me'
-                user_info_fields = [
-                    'id', 'first_name', 'last_name', 'picture', 'email', 
-                ]
-                params_user_info = {
-                    "fields": ','.join(user_info_fields),
-                    "access_token": token_json.get("access_token")
-                }
-                profile_request = requests.get(url_user_info, params=params_user_info)
-                profile_json = profile_request.json()
-                print(profile_json)
-                email = profile_json.get("email", None)
-                if email is not None:
-                    first_name = profile_json.get("first_name")
-                    last_name = profile_json.get("last_name")
-                    email = profile_json.get("email")
-                    try:
-                        user = models.User.objects.get(email=email)
-                        if user.login_method != models.User.LOGIN_FACEBOOK:
-                            raise FacebookException(
-                                f"Please log in with: {user.login_method}"
-                            )
-                    except models.User.DoesNotExist:
-                        user = models.User.objects.create(
-                            email=email,
-                            first_name=first_name,
-                            last_name=last_name,
-                            username=email,
-                            login_method=models.User.LOGIN_FACEBOOK,
-                            email_verified=True,
-                        )
-                        user.set_unusable_password()
-                        user.save()
-                    login(request, user)
-                    messages.success(request, f"Welcome back {user.first_name}")
-                    return redirect(reverse("core:home"))
-                else:
-                    raise FacebookException("Can't get your profile")
-        else:
-            raise FacebookException("Can't get code")
-    except FacebookException as e:
-        messages.error(request, e)
-        return redirect(reverse("users:login"))
+# def facebook_callback(request):
+#     try:
+#         client_id = os.environ.get("FACEBOOK_ID")
+#         client_secret = os.environ.get("FACEBOOK_SECRET")
+#         redirect_uri = "http://airbnp.eba-8dydnzzj.ap-northeast-2.elasticbeanstalk.com/users/login/facebook/callback"
+#         code = request.GET.get("code", None)
+#         if code is not None:
+#             token_request = requests.post(
+#                 f"https://graph.facebook.com/v6.0/oauth/access_token?client_id={client_id}&redirect_uri={redirect_uri}&client_secret={client_secret}&code={code}",
+#                 headers={"Accept": "application/json"},
+#             )
+#             token_json = token_request.json()
+#             error = token_json.get("error", None)
+#             if error is not None:
+#                 raise FacebookException("Can't get access token")
+#             else:
+#                 url_user_info = 'https://graph.facebook.com/me'
+#                 user_info_fields = [
+#                     'id', 'first_name', 'last_name', 'picture', 'email', 
+#                 ]
+#                 params_user_info = {
+#                     "fields": ','.join(user_info_fields),
+#                     "access_token": token_json.get("access_token")
+#                 }
+#                 profile_request = requests.get(url_user_info, params=params_user_info)
+#                 profile_json = profile_request.json()
+#                 print(profile_json)
+#                 email = profile_json.get("email", None)
+#                 if email is not None:
+#                     first_name = profile_json.get("first_name")
+#                     last_name = profile_json.get("last_name")
+#                     email = profile_json.get("email")
+#                     try:
+#                         user = models.User.objects.get(email=email)
+#                         if user.login_method != models.User.LOGIN_FACEBOOK:
+#                             raise FacebookException(
+#                                 f"Please log in with: {user.login_method}"
+#                             )
+#                     except models.User.DoesNotExist:
+#                         user = models.User.objects.create(
+#                             email=email,
+#                             first_name=first_name,
+#                             last_name=last_name,
+#                             username=email,
+#                             login_method=models.User.LOGIN_FACEBOOK,
+#                             email_verified=True,
+#                         )
+#                         user.set_unusable_password()
+#                         user.save()
+#                     login(request, user)
+#                     messages.success(request, f"Welcome back {user.first_name}")
+#                     return redirect(reverse("core:home"))
+#                 else:
+#                     raise FacebookException("Can't get your profile")
+#         else:
+#             raise FacebookException("Can't get code")
+#     except FacebookException as e:
+#         messages.error(request, e)
+#         return redirect(reverse("users:login"))
 
 
 class UserProfileView(DetailView):
