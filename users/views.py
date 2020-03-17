915522ece3f3694e65bdb727503e447921f161a3
plusbeauxjours <plusbeauxjours@gmail.com>
2020-03-17 17:23:49 +0900

    https => http

--- users/views.py
+++ users/views.py
@@ -208,7 +208,7 @@ def kakao_callback(request):
 
 def facebook_login(request):
     client_id = os.environ.get("FACEBOOK_ID")
-    redirect_uri = "https://airbnp.eba-8dydnzzj.ap-northeast-2.elasticbeanstalk.com/users/login/facebook/callback"
+    redirect_uri = "http://airbnp.eba-8dydnzzj.ap-northeast-2.elasticbeanstalk.com/users/login/facebook/callback"
     response_type = "code"
     return redirect(
         f"https://www.facebook.com/v6.0/dialog/oauth?client_id={client_id}&redirect_uri={redirect_uri}&response_type={response_type}",
@@ -223,7 +223,7 @@ def facebook_callback(request):
     try:
         client_id = os.environ.get("FACEBOOK_ID")
         client_secret = os.environ.get("FACEBOOK_SECRET")
-        redirect_uri = "https://airbnp.eba-8dydnzzj.ap-northeast-2.elasticbeanstalk.com/users/login/facebook/callback"
+        redirect_uri = "http://airbnp.eba-8dydnzzj.ap-northeast-2.elasticbeanstalk.com/users/login/facebook/callback"
         code = request.GET.get("code", None)
         if code is not None:
             token_request = requests.post(
