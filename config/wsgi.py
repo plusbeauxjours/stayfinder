43596e34a2dea60b7288744046d40725c695b17c
plusbeauxjours <plusbeauxjours@gmail.com>
2020-04-15 22:15:55 +0900

    deploy to Heroku

--- config/wsgi.py
+++ config/wsgi.py
@@ -6,13 +6,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
 
 application = DjangoWhiteNoise(application)
