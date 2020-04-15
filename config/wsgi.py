9816e6db08f79a96e475b8c9e33eb8423b364a1d
plusbeauxjours <plusbeauxjours@gmail.com>
2020-04-15 23:29:46 +0900

    deploy to Heroku

--- config/wsgi.py
+++ config/wsgi.py
@@ -6,11 +6,14 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise.django import DjangoWhiteNoise
+import sys
 from django.core.wsgi import get_wsgi_application
+
+app_path = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+)
+sys.path.append(os.path.join(app_path, "airbnp"))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
-
-application = DjangoWhiteNoise(application)
