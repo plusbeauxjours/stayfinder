fcbf4a559ab3347eceebc6be78a6c717d904998f
plusbeauxjours <plusbeauxjours@gmail.com>
2020-04-16 00:55:04 +0900

    done

--- config/wsgi.py
+++ config/wsgi.py
@@ -6,13 +6,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
-from django.core.wsgi import get_wsgi_application
 
-app_path = os.path.abspath(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
-)
-sys.path.append(os.path.join(app_path, "airbnp"))
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
